f4abca83ea2b373503aecda38eaaef9833d772ca
Nikitas15K <60483876+Nikitas15K@users.noreply.github.com>
2020-06-28 04:42:58 +0300

    Delete graph_analysis.py

--- graph_analysis.py
+++
@@ -1,7 +0,0 @@
-from functions import calculates_max_metrics, calculates_general_metrics, calculates_average_metrics
-from graphs import graph_list, name_list
-
-for i in range(0, len(graph_list)):
-    calculates_general_metrics(graph_list[i], name_list[i])
-    calculates_max_metrics(graph_list[i], name_list[i])
-    calculates_average_metrics(graph_list[i], name_list[i])
